8bbed9a1077b0d917c036d5e725234e2e4a1a32d
Mechardo <lucas_rack@live.com.ar>
2025-07-28 13:32:56 -0300

    Add handshake and test

--- src-tauri/src/backend/discord_async/error.rs
+++ src-tauri/src/backend/discord_async/error.rs
@@ -1,15 +1,43 @@
-#[derive(Debug, Clone)]
+use thiserror::Error;
+
+#[derive(Error, Debug)]
 pub enum DiscordError {
+    #[error("Failed to connect to the Discord pipe")]
     PipeConnectionFailed,
+
+    #[error("Discord pipe is not connected")]
     PipeNotConnected,
+
+    #[error("Error reading from the Discord pipe")]
     PipeErrorReading,
+
+    #[error("Error writing to the Discord pipe")]
     PipeWriteError,
+
+    #[error("Discord handshake failed")]
     HandshakeFailed,
+
+    #[error("Client ID not found")]
     ClientIdNotFound,
+
+    #[error("Failed to convert data with Serde")]
     SerdeConvertionError,
+
+    #[error("Authorization with Discord failed")]
     AuthorizationFailed,
+
+    #[error("Authentication with Discord failed")]
     AuthenticationFailed,
+
+    #[error("No data found in the response")]
     NoDataFound,
+
+    #[error("Internal communication channel is closed")]
     InternalChannelClosed,
+
+    #[error("Error closing the thread")]
     ErrorClosingThread,
+
+    #[error("Pipe read error: {0}")]
+    PipeReadError(#[from] std::io::Error),
 }
--- src-tauri/src/backend/discord_async/ipc.rs
+++ src-tauri/src/backend/discord_async/ipc.rs
@@ -1,16 +1,17 @@
 use tokio::io::AsyncReadExt;
+
+use tokio::io::AsyncWriteExt;
 #[cfg(windows)]
 use tokio::net::windows::named_pipe::{ClientOptions, NamedPipeClient};
-// use named_pipe::PipeClient;
-use serde_json::Value;
-use std::collections::HashMap;
-use std::io::{Read, Write};
+
 #[cfg(unix)]
 use std::{env::var, os::unix::net::UnixStream};
 
 use crate::error::DiscordError;
 
+use discord::pipemessage::Opcode;
 use discord::pipemessage::PipeMessage;
+
 pub struct IpcClient {
     #[cfg(unix)]
     pub pipe_client: Option<UnixStream>,
@@ -19,6 +20,7 @@ pub struct IpcClient {
     pub pipe_client: Option<NamedPipeClient>,
 
     pub client_id: Option<String>,
+    pub connected: bool,
 }
 
 impl IpcClient {
@@ -26,6 +28,7 @@ impl IpcClient {
         Self {
             pipe_client: None,
             client_id: None,
+            connected: false,
         }
     }
 
@@ -37,8 +40,6 @@ impl IpcClient {
             if let Ok(pipe) = ClientOptions::new().open(pipe_name) {
                 self.pipe_client = Some(pipe);
                 return Ok(());
-            } else {
-                continue;
             }
         }
 
@@ -50,48 +51,87 @@ impl IpcClient {
         let received_opcode: u32;
         let received_length: u32;
 
-        let Some(pipe_client) = &self.pipe_client else {
+        let Some(pipe_client) = &mut self.pipe_client else {
             return Err(DiscordError::PipeNotConnected);
         };
 
-        // pipe_client.read_exact(&mut buf).await?;
+        pipe_client.read_exact(&mut buf).await?;
 
-        return Err(DiscordError::PipeNotConnected);
+        received_opcode = u32::from_le_bytes(buf);
 
-        // match &mut self.client_pipe {
-        //     Some(cp) => {
-        //         cp.read_exact(&mut buf)
-        //             .map_err(|_| DiscordError::PipeErrorReading)?;
-        //         received_opcode = u32::from_le_bytes(buf);
+        pipe_client.read_exact(&mut buf).await?;
 
-        //         cp.read_exact(&mut buf)
-        //             .map_err(|_| DiscordError::PipeErrorReading)?;
-        //         received_length = u32::from_le_bytes(buf);
+        received_length = u32::from_le_bytes(buf);
 
-        //         let mut response_data = vec![0u8; received_length as usize];
-        //         cp.read_exact(&mut response_data)
-        //             .map_err(|_| DiscordError::PipeErrorReading)?;
-        //         let response_data_str = String::from_utf8_lossy(&response_data);
+        let response_data = vec![0u8; received_length as usize];
 
-        //         return Ok(PipeMessage::new(
-        //             Opcode::new(received_opcode),
-        //             &response_data_str,
-        //         ));
-        //     }
-        //     None => return Err(DiscordError::PipeNotConnected),
-        // }
+        let response_data_str = String::from_utf8_lossy(&response_data);
+
+        return Ok(PipeMessage::new(
+            Opcode::new(received_opcode),
+            &response_data_str,
+        ));
+    }
+
+    pub async fn handshake(&mut self, client_id: String) -> Result<(), DiscordError> {
+        let Some(pipe_client) = &mut self.pipe_client else {
+            return Err(DiscordError::PipeNotConnected);
+        };
+        //store client id
+        self.client_id = Some(client_id.clone());
+
+        pipe_client
+            .write_all(&PipeMessage::handshake(&client_id).to_buff())
+            .await?;
+
+        if self.read_message().await?.opcode != Opcode::Frame {
+            return Err(DiscordError::HandshakeFailed);
+        }
+
+        self.connected = true;
+        return Ok(());
     }
 }
 
-// for running these test, discord is needed to be running on the background
+// for running these tests, discord should be running on the background
 #[cfg(test)]
 mod tests {
     use super::IpcClient;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    use std::path::PathBuf;
+
+    fn load_env_file() {
+        let env_file_path = PathBuf::from("../../../../discord.env");
+
+        let reader = BufReader::new(File::open(env_file_path).unwrap());
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if line.starts_with("#") {
+                // Skip comments
+                continue;
+            };
+            match line.split_once('=') {
+                Some((key, value)) => unsafe { std::env::set_var(key, value) },
+                None => continue,
+            };
+        }
+    }
 
     #[tokio::test]
     async fn test_basic_connect() {
-        let mut ipc_client = IpcClient::new();
+        load_env_file();
+        let mut ipc_client: IpcClient = IpcClient::new();
+
+        let client_id = std::env::var("DISCORD_CLIENT_ID").unwrap();
+
         ipc_client.connect().unwrap();
         assert!(ipc_client.pipe_client.is_some());
+
+        ipc_client.handshake(client_id).await.unwrap();
+
+        assert!(ipc_client.connected);
     }
 }
