5aaf6902a6b94eb50f14cdb7fa26a5b98888db36
Mechardo <lucas_rack@live.com.ar>
2025-08-06 20:46:17 -0300

    Init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,11 @@
 use controller::commands;
 use controller::controller::Controller;
 use std::sync::Arc;
+use tauri::menu::{Menu, MenuItem};
+use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
 use tauri::{Emitter, Manager};
 use tokio::sync::Mutex;
-use tracing::info;
+use tracing::{debug, info};
 
 #[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #[tokio::main]
@@ -34,25 +36,62 @@ async fn main() {
         ])
         .on_window_event(|window, event| match event {
             tauri::WindowEvent::CloseRequested { api, .. } => {
-                info!("Closing app");
+                debug!("Hiding app");
                 api.prevent_close();
-                let app = window.app_handle().clone();
-                let controller = app.state::<Arc<Mutex<Controller>>>().inner().clone();
-                let shutdown_complete = app.state::<Arc<Mutex<bool>>>().inner().clone();
-                tauri::async_runtime::spawn(async move {
-                    controller
-                        .lock()
-                        .await
-                        .shutdown()
-                        .await
-                        .expect("Error shutting down controller");
-                    *shutdown_complete.lock().await = true;
-                    app.emit_to("main", "shutdown-complete", ()).unwrap();
-                });
+                window.hide().unwrap();
             }
             _ => {}
         })
         .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Salir", true, None::<&str>)?;
+            let show_i = MenuItem::with_id(app, "show", "Abrir DS2000", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i, &show_i])?;
+
+            TrayIconBuilder::new()
+                .menu(&menu)
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_menu_event(|app_handle, event| match event.id.as_ref() {
+                    "quit" => {
+                        let app = app_handle.clone();
+                        let controller = app.state::<Arc<Mutex<Controller>>>().inner().clone();
+                        let shutdown_complete = app.state::<Arc<Mutex<bool>>>().inner().clone();
+                        tauri::async_runtime::spawn(async move {
+                            controller
+                                .lock()
+                                .await
+                                .shutdown()
+                                .await
+                                .expect("Error shutting down controller");
+                            *shutdown_complete.lock().await = true;
+                            app.emit_to("main", "shutdown-complete", ()).unwrap();
+                        });
+                    }
+                    "show" => {
+                        if let Some(window) = app_handle.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        debug!("left click pressed and released");
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
+                .build(app)?;
             let app_handle = app.handle().clone();
             let shutdown_complete = app.state::<Arc<Mutex<bool>>>().inner().clone();
             std::thread::spawn(move || {
